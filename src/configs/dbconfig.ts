c1a708f27fc93ac7d4d8a26f59bada7f56ecc8a6
Destiny <onahdestinyc20@gmail.com>
2023-10-12 01:31:10 +0100

    SetUp for social projeect

---
+++ src/configs/dbconfig.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.MYSQL_URL!, {
+  database: process.env.MYSQLDATABASE,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  host: process.env.MYSQLHOST,
+  port: <any>process.env.MYSQLPORT,
+  dialect: "mysql",
+  define: {
+    timestamps: true
+  }
+});
+
+export default sequelize;
